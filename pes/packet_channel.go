c3901ea2ea5913c711f65f1f3b7fa9d9b5b356e5
James Holmes <32bitkid@gmail.com>
2014-08-19 17:08:59 -0400

    Use defer to schedule channel closing

--- pes/packet_channel.go
+++ pes/packet_channel.go
@@ -5,10 +5,10 @@ type PacketChannel <-chan *Packet
 func (input PacketChannel) PayloadOnly() <-chan []byte {
 	output := make(chan []byte)
 	go func() {
+		defer close(output)
 		for packet := range input {
 			output <- packet.Payload
 		}
-		close(output)
 	}()
 	return output
 }
