09e291e1e550393ab9cb5e7b9d8655cfa659d0fa
James Holmes <32bitkid@gmail.com>
2015-12-10 20:47:43 -0700

    adding `SkipUntil` to align into a TS stream.

--- ts/payload_reader.go
+++ ts/payload_reader.go
@@ -6,27 +6,50 @@ import "github.com/32bitkid/mpeg/util"
 
 type PayloadReader interface {
 	io.Reader
+	SkipUntil(PacketTester) error
 }
 
 func NewPayloadReader(source io.Reader, tester PacketTester) PayloadReader {
 	return &payloadReader{
-		packet: new(Packet),
-		tsr:    util.NewSimpleBitReader(source),
-		tester: tester,
+		currentPacket: nil,
+		tsr:           util.NewSimpleBitReader(source),
+		tester:        tester,
 	}
 }
 
 type payloadReader struct {
-	packet    *Packet
-	tsr       util.BitReader32
-	tester    PacketTester
-	remainder bytes.Buffer
+	currentPacket *Packet
+	tsr           util.BitReader32
+	tester        PacketTester
+	remainder     bytes.Buffer
+}
+
+func (r *payloadReader) Next() error {
+	if r.currentPacket == nil {
+		r.currentPacket = new(Packet)
+	}
+	return r.currentPacket.ReadFrom(r.tsr)
+}
+
+func (r *payloadReader) SkipUntil(begin PacketTester) (err error) {
+	r.remainder.Reset()
+	t := r.tester.And(begin)
+	for {
+		r.Next()
+		if err != nil {
+			return err
+		}
+		if t(r.currentPacket) {
+			r.remainder.Write(r.currentPacket.Payload)
+			return nil
+		}
+	}
+	return io.EOF
 }
 
 func (r *payloadReader) Read(p []byte) (n int, err error) {
 
 	var remainder []byte
-	packet := r.packet
 
 	if r.remainder.Len() > 0 {
 		copied, err := r.remainder.Read(p)
@@ -38,15 +61,15 @@ func (r *payloadReader) Read(p []byte) (n int, err error) {
 	}
 
 	for len(p) > 0 {
-		err = packet.ReadFrom(r.tsr)
+		err = r.Next()
 		if err != nil {
 			return
 		}
 
-		copied := copy(p, packet.Payload)
+		copied := copy(p, r.currentPacket.Payload)
 		n = n + copied
 		p = p[copied:]
-		remainder = packet.Payload[copied:]
+		remainder = r.currentPacket.Payload[copied:]
 	}
 
 	_, err = r.remainder.Write(remainder)
