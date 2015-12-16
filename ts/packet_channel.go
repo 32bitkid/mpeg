922e2b1664ca7e110c6303170b94683b07cc5aa7
James Holmes <32bitkid@gmail.com>
2015-12-16 08:41:31 -0700

    PacketChannels can be transformed into a channel of PayloadUnits

--- ts/packet_channel.go
+++ ts/packet_channel.go
@@ -1,5 +1,8 @@
 package ts
 
+import "bytes"
+import "io/ioutil"
+
 type PacketChannel <-chan *Packet
 
 func (input PacketChannel) PayloadOnly() <-chan []byte {
@@ -8,6 +11,30 @@ func (input PacketChannel) PayloadOnly() <-chan []byte {
 		defer close(output)
 		for packet := range input {
 			output <- packet.Payload
+		}
+	}()
+	return output
+}
+
+func (input PacketChannel) PayloadUnit() <-chan []byte {
+	var buf bytes.Buffer
+	output := make(chan []byte)
+	started := false
+	go func() {
+		defer close(output)
+		for packet := range input {
+			buf.Write(packet.Payload)
+
+			if packet.PayloadUnitStartIndicator {
+				if started {
+					data, err := ioutil.ReadAll(&buf)
+					if err != nil {
+						output <- data
+					}
+				} else {
+					started = true
+				}
+			}
 		}
 	}()
 	return output
