f70c1f225c706f65262e21bb37e87c5ad7625e23
James Holmes <32bitkid@gmail.com>
2015-12-17 15:37:51 -0700

    updating start codes

--- video/start_codes.go
+++ video/start_codes.go
@@ -11,21 +11,23 @@ const (
 const (
 	StartCodePrefix = 0x000001
 
-	PictureStartCode    = 0x00
-	ReservedStartCode_1 = 0xB0
-	ReservedStartCode_2 = 0xB1
-	UserDataStartCode   = 0xB2
-	SequenceHeaderCode  = 0xB3
-	SequenceErrorCode   = 0xB4
-	ExtensionCode       = 0xB5
-	ReservedStartCode_3 = 0xB6
-	SequenceEndCode     = 0xB7
-	GroupCode           = 0xB8
+	PictureCode        = 0x00
+	ReservedCode_1     = 0xB0
+	ReservedCode_2     = 0xB1
+	UserDataCode       = 0xB2
+	SequenceHeaderCode = 0xB3
+	SequenceErrorCode  = 0xB4
+	ExtensionCode      = 0xB5
+	ReservedCode_3     = 0xB6
+	SequenceEndCode    = 0xB7
+	GroupCode          = 0xB8
 
 	SequenceHeaderStartCode StartCode = (StartCodePrefix << 8) + SequenceHeaderCode
 	ExtensionStartCode                = (StartCodePrefix << 8) + ExtensionCode
 	SequenceEndStartCode              = (StartCodePrefix << 8) + SequenceEndCode
 	GroupStartCode                    = (StartCodePrefix << 8) + GroupCode
+	PictureStartCode                  = (StartCodePrefix << 8) + PictureCode
+	UserDataStartCode                 = (StartCodePrefix << 8) + UserDataCode
 )
 
 // slice_start_code 01 through AF
