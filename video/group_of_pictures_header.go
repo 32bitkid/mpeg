74b8d316aade81a74c271ffd33125591998f3670
James Holmes <32bitkid@gmail.com>
2015-12-17 15:18:55 -0700

    GOP implementation.

--- video/group_of_pictures_header.go
+++ video/group_of_pictures_header.go
@@ -2,6 +2,34 @@ package video
 
 import "github.com/32bitkid/mpeg/util"
 
-func group_of_pictures_header(br util.BitReader32) {
-	panic("not supported: group_of_pictures_header")
+type GroupOfPicturesHeader struct {
+	time_code   uint32 // 25 bslbf
+	closed_gop  bool   // 1 uimsbf
+	broken_link bool   // 1 uimsbf
+
+}
+
+func group_of_pictures_header(br util.BitReader32) (*GroupOfPicturesHeader, error) {
+	err := start_code_check(br, GroupStartCode)
+	if err != nil {
+		return nil, err
+	}
+
+	goph := GroupOfPicturesHeader{}
+	goph.time_code, err = br.Read32(25)
+	if err != nil {
+		return nil, err
+	}
+
+	goph.closed_gop, err = br.ReadBit()
+	if err != nil {
+		return nil, err
+	}
+
+	goph.broken_link, err = br.ReadBit()
+	if err != nil {
+		return nil, err
+	}
+
+	return &goph, next_start_code(br)
 }
