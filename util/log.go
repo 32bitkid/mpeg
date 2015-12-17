1e8c9236d38e410742a28c21636c67069650e640
James Holmes <32bitkid@gmail.com>
2015-12-17 12:59:37 -0700

    Logging utility

---
+++ util/log.go
@@ -0,0 +1,37 @@
+package util
+
+import "log"
+import "io/ioutil"
+import "os"
+import "strings"
+
+var namespaces = strings.Split(os.Getenv("DEBUG"), ",")
+
+type Logger interface {
+	Print(...interface{})
+	Printf(string, ...interface{})
+	Println(...interface{})
+}
+
+var NoopLogger = log.New(ioutil.Discard, "", 0)
+
+func NewLog(ns string) Logger {
+	for _, namespace := range namespaces {
+		if namespace == "*" {
+			return createLogger(ns)
+		}
+
+		if strings.HasSuffix(namespace, ":*") && strings.HasPrefix(ns, namespace[:len(namespace)-2]) {
+			return createLogger(ns)
+		}
+
+		if ns == namespace {
+			return createLogger(ns)
+		}
+	}
+	return NoopLogger
+}
+
+func createLogger(ns string) Logger {
+	return log.New(os.Stdout, ns+": ", 0)
+}
