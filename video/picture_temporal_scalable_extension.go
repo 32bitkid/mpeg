0cbfe1cd0a1b4c27a5b319403bc4840c20f85b61
James Holmes <32bitkid@gmail.com>
2015-12-17 16:16:19 -0700

    support `picture_temporal_scalable_extension`

--- video/picture_temporal_scalable_extension.go
+++ video/picture_temporal_scalable_extension.go
@@ -2,6 +2,40 @@ package video
 
 import "github.com/32bitkid/mpeg/util"
 
-func picture_temporal_scalable_extension(br util.BitReader32) {
-	panic("unsupported: picture_temporal_scalable_extension")
+type PictureTemporalScalableExtension struct {
+	reference_select_code       uint32 // 2 uimsbf
+	forward_temporal_reference  uint32 // 10 uimsbf
+	backward_temporal_reference uint32 // 10 uimsbf
+}
+
+func picture_temporal_scalable_extension(br util.BitReader32) (*PictureTemporalScalableExtension, error) {
+	err := extension_code_check(br, PictureTemporalScalableExtensionID)
+	if err != nil {
+		return nil, err
+	}
+
+	ptse := PictureTemporalScalableExtension{}
+
+	ptse.reference_select_code, err = br.Read32(2)
+	if err != nil {
+		return nil, err
+	}
+
+	ptse.forward_temporal_reference, err = br.Read32(10)
+	if err != nil {
+		return nil, err
+	}
+
+	err = marker_bit(br)
+	if err != nil {
+		return nil, err
+	}
+
+	ptse.backward_temporal_reference, err = br.Read32(10)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ptse, next_start_code(br)
+
 }
