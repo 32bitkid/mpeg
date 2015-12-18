3444ff0b531387c6a8641f426ca832d5153f318b
James Holmes <32bitkid@gmail.com>
2015-12-18 09:54:47 -0700

    using huffman decoder to read macroblock_address_increment

--- video/macroblock.go
+++ video/macroblock.go
@@ -2,8 +2,31 @@ package video
 
 import "github.com/32bitkid/mpeg/util"
 
-type Macroblock struct{}
+type Macroblock struct {
+	macroblock_address_increment uint32
+	macroblock_type              *MacroblockType
+}
 
 func macroblock(br util.BitReader32) (*Macroblock, error) {
-	panic("not implemented: macroblock")
+
+	mb := Macroblock{}
+
+	for {
+		nextbits, err := br.Peek32(11)
+		if err != nil {
+			return nil, err
+		}
+		if nextbits == 0x08 { // 0000 0001 000
+			br.Trash(11)
+			mb.macroblock_address_increment += 33
+		}
+
+		incr, err := MacroblockAddressIncrementDecoder.Decode(br)
+		if err != nil {
+			return nil, err
+		}
+		mb.macroblock_address_increment += incr
+
+		panic("not implemented: macroblock")
+	}
 }
