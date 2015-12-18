b4afec56be05de75193d07107e964f073a668b7b
James Holmes <32bitkid@gmail.com>
2015-12-18 09:55:41 -0700

    starting implementation for macroblock_type decoder
    need to get picture type from last decoded `picture_header` to select the correct header, needs some rearchitecting.

---
+++ video/macroblock_type.go
@@ -0,0 +1,78 @@
+package video
+
+import "github.com/32bitkid/mpeg/util"
+
+type macroblockTypeDecoder func(util.BitReader32) (*MacroblockType, error)
+
+func newMacroblockTypeDecoder(table util.HuffmanTable) macroblockTypeDecoder {
+	decoder := util.NewHuffmanDecoder(table)
+	return func(br util.BitReader32) (*MacroblockType, error) {
+		val, err := decoder.Decode(br)
+		if err != nil {
+			return nil, err
+		} else if mb_type, ok := val.(*MacroblockType); ok {
+			return mb_type, nil
+		} else {
+			return nil, ErrUnexpectedDecodedValueType
+		}
+	}
+}
+
+type SpatialTemporalWeightClass int
+
+const (
+	SpatialTemporalWeightClass_0 = 1 << iota
+	SpatialTemporalWeightClass_1
+	SpatialTemporalWeightClass_2
+	SpatialTemporalWeightClass_3
+	SpatialTemporalWeightClass_4
+)
+
+type MacroblockType struct {
+	macroblock_quant                  bool
+	macroblock_motion_forward         bool
+	macroblock_motion_backward        bool
+	macroblock_pattern                bool
+	macroblock_intra                  bool
+	spatial_temporal_weight_code_flag bool
+	spatial_temporal_weight_classes   SpatialTemporalWeightClass
+}
+
+var iFrameMacroblockTypesTable = util.HuffmanTable{
+	{"1", &MacroblockType{false, false, false, false, true, false, SpatialTemporalWeightClass_0}},
+	{"01", &MacroblockType{true, false, false, false, true, false, SpatialTemporalWeightClass_0}},
+}
+
+var pFrameMacroblockTypesTable = util.HuffmanTable{
+	{"1", &MacroblockType{false, true, false, true, false, false, SpatialTemporalWeightClass_0}},
+	{"01", &MacroblockType{false, false, false, true, false, false, SpatialTemporalWeightClass_0}},
+	{"001", &MacroblockType{false, true, false, false, false, false, SpatialTemporalWeightClass_0}},
+	{"0001 1", &MacroblockType{false, false, false, false, true, false, SpatialTemporalWeightClass_0}},
+	{"0001 0", &MacroblockType{true, true, false, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0000 1", &MacroblockType{true, false, false, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0000 01", &MacroblockType{true, false, false, false, true, false, SpatialTemporalWeightClass_0}},
+}
+
+var bFrameMacroblockTypesTable = util.HuffmanTable{
+	{"10", &MacroblockType{false, true, true, false, false, false, SpatialTemporalWeightClass_0}},
+	{"11", &MacroblockType{false, true, true, true, false, false, SpatialTemporalWeightClass_0}},
+	{"010", &MacroblockType{false, false, true, false, false, false, SpatialTemporalWeightClass_0}},
+	{"011", &MacroblockType{false, false, true, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0010", &MacroblockType{false, true, false, false, false, false, SpatialTemporalWeightClass_0}},
+	{"0011", &MacroblockType{false, true, false, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0001 1", &MacroblockType{false, false, false, false, true, false, SpatialTemporalWeightClass_0}},
+	{"0001 0", &MacroblockType{true, true, true, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0000 11", &MacroblockType{true, true, false, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0000 10", &MacroblockType{true, false, true, true, false, false, SpatialTemporalWeightClass_0}},
+	{"0000 01", &MacroblockType{true, false, false, false, true, false, SpatialTemporalWeightClass_0}},
+}
+
+var MacroblockTypeDecoder = struct {
+	IFrame macroblockTypeDecoder
+	PFrame macroblockTypeDecoder
+	BFrame macroblockTypeDecoder
+}{
+	newMacroblockTypeDecoder(iFrameMacroblockTypesTable),
+	newMacroblockTypeDecoder(pFrameMacroblockTypesTable),
+	newMacroblockTypeDecoder(bFrameMacroblockTypesTable),
+}
