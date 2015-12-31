a99313a6f0835ed9f5cdaf7dc6962f7e4adfc809
James Holmes <32bitkid@gmail.com>
2015-12-31 13:34:28 -0700

    updating summary

--- pes/pes.go
+++ pes/pes.go
@@ -1,2 +1,8 @@
-// Package pes provides support for parsing and traversing streams of MPEG-2 PES data
+// Package pes implements an MPEG-2 packetized elementary stream parser as defined in ISO/IEC 13818-1.
+//
+// *Note* This package is a work-in-progress, and even when complete intended for educational purposes only.
+//
+// This package is _not_ optimized for processing video in a production environment, instead is
+// geared towards an educational look at some of the algorithms and processes used to decode
+// compressed video.
 package pes
