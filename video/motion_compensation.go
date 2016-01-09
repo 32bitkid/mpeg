8ea4564ba9cc5015f11a385eff06b027277d660a
James Holmes <32bitkid@gmail.com>
2016-01-09 12:43:03 -0700

    reworking the motion compensation stuff

--- video/motion_compensation.go
+++ video/motion_compensation.go
@@ -1,67 +1,145 @@
 package video
 
+type motionCompensationState struct {
+	i       int
+	channel []uint8
+	stride  int
+	h_half  bool
+	v_half  bool
+}
+
 func (vs *VideoSequence) motion_compensation(motionVectors *motionVectorData, i, mb_row, mb_addr int, mb *Macroblock, b *block) {
 
-	if vs.PictureHeader.picture_coding_type == PFrame {
+	if mb.macroblock_type.macroblock_intra {
+		return
+	}
 
-		if !mb.macroblock_type.macroblock_intra {
+	var (
+		hasB   = mb.macroblock_type.macroblock_motion_backward && vs.frameStore.future != nil
+		hasF   = (mb.macroblock_type.macroblock_motion_forward || vs.PictureHeader.picture_coding_type == PFrame) && vs.frameStore.past != nil
+		bState motionCompensationState
+		fState motionCompensationState
+	)
 
-			horizontal, vertical := motionVectors.actual[0][0][0], motionVectors.actual[0][0][1]
+	// project future image _backward_...
+	if hasB {
+		horizontal, vertical := motionVectors.actual[0][1][0], motionVectors.actual[0][1][1]
 
-			// Scale Cb/Cr vectors
-			if i >= 4 {
-				horizontal >>= 1
-				vertical >>= 1
-			}
+		// Scale Cb/Cr vectors
+		if i >= 4 {
+			horizontal >>= 1
+			vertical >>= 1
+		}
 
-			// is half?
-			h_half, v_half := (horizontal&1) == 1, (vertical&1) == 1
+		// is half pel?
+		bState.h_half, bState.v_half = (horizontal&1) == 1, (vertical&1) == 1
+		// scale down by half
+		horizontal, vertical = horizontal>>1, vertical>>1
 
-			var (
-				srcX    int
-				srcY    int
-				channel []uint8
-				stride  int
-			)
+		image := vs.frameStore.future
 
-			image := vs.frameStore.past
+		switch i {
+		case 0, 1, 2, 3:
+			bState.channel = image.Y
+			bState.stride = image.YStride
+			bState.i = (((mb_row * 16) + (i&2)<<2) * bState.stride) + (mb_addr * 16) + (i&1)<<3
+		case 4:
+			bState.channel = image.Cb
+			bState.stride = image.CStride
+			bState.i = (mb_row * 8 * bState.stride) + (mb_addr * 8)
+		case 5:
+			bState.channel = image.Cr
+			bState.stride = image.CStride
+			bState.i = (mb_row * 8 * bState.stride) + (mb_addr * 8)
+		}
 
-			switch i {
-			case 0, 1, 2, 3:
-				channel = image.Y
-				stride = image.YStride
-				srcX = (mb_addr * 16) + (i&1)<<3
-				srcY = (mb_row * 16) + (i&2)<<2
-			case 4:
-				channel = image.Cb
-				stride = image.CStride
-				srcX = mb_addr * 8
-				srcY = mb_row * 8
-			case 5:
-				channel = image.Cr
-				stride = image.CStride
-				srcX = mb_addr * 8
-				srcY = mb_row * 8
-			}
+		bState.i += vertical * bState.stride
+		bState.i += horizontal
+	}
 
-			srcX += horizontal >> 1
-			srcY += vertical >> 1
+	// project past image _forward_...
+	if hasF {
+		horizontal, vertical := motionVectors.actual[0][0][0], motionVectors.actual[0][0][1]
 
-			for v := 0; v < 8; v++ {
-				for u := 0; u < 8; u++ {
-					i := ((srcY + v) * stride) + (srcX + u)
-					switch {
-					case !h_half && !v_half:
-						b[v*8+u] += int32(channel[i])
-					case h_half && !v_half:
-						b[v*8+u] += (int32(channel[i]) + int32(channel[i+1])) / 2
-					case !h_half && v_half:
-						b[v*8+u] += (int32(channel[i]) + int32(channel[i+stride])) / 2
-					case h_half && v_half:
-						b[v*8+u] += (int32(channel[i]) + int32(channel[i+1]) + int32(channel[i+stride]) + int32(channel[i+stride+1])) / 4
-					}
+		// Scale Cb/Cr vectors
+		if i >= 4 {
+			horizontal >>= 1
+			vertical >>= 1
+		}
+
+		// is half pel?
+		fState.h_half, fState.v_half = (horizontal&1) == 1, (vertical&1) == 1
+		// scale down by half
+		horizontal, vertical = horizontal>>1, vertical>>1
+
+		image := vs.frameStore.past
+
+		switch i {
+		case 0, 1, 2, 3:
+			fState.channel = image.Y
+			fState.stride = image.YStride
+			fState.i = (((mb_row * 16) + (i&2)<<2) * fState.stride) + (mb_addr * 16) + (i&1)<<3
+		case 4:
+			fState.channel = image.Cb
+			fState.stride = image.CStride
+			fState.i = (mb_row * 8 * fState.stride) + (mb_addr * 8)
+		case 5:
+			fState.channel = image.Cr
+			fState.stride = image.CStride
+			fState.i = (mb_row * 8 * fState.stride) + (mb_addr * 8)
+		}
+
+		fState.i += vertical * fState.stride
+		fState.i += horizontal
+	}
+
+	for v := 0; v < 8; v++ {
+		for u := 0; u < 8; u++ {
+			var pel int32 = 0
+			var samples uint = 0
+			if hasF {
+				i := fState.i + (v * fState.stride) + u
+				samples++
+				switch {
+				case !fState.h_half && !fState.v_half:
+					pel += int32(fState.channel[i])
+
+				case fState.h_half && !fState.v_half:
+					pel += (int32(fState.channel[i]) +
+						int32(fState.channel[i+1])) / 2
+
+				case !fState.h_half && fState.v_half:
+					pel += (int32(fState.channel[i]) +
+						int32(fState.channel[i+fState.stride])) / 2
+
+				case fState.h_half && fState.v_half:
+					pel += (int32(fState.channel[i]) +
+						int32(fState.channel[i+1]) + int32(fState.channel[i+fState.stride]) + int32(fState.channel[i+fState.stride+1])) / 4
 				}
 			}
+			if hasB {
+				i := bState.i + (v * bState.stride) + u
+				samples++
+				switch {
+				case !bState.h_half && !bState.v_half:
+					pel += int32(bState.channel[i])
+
+				case bState.h_half && !bState.v_half:
+					pel += (int32(bState.channel[i]) +
+						int32(bState.channel[i+1])) / 2
+
+				case !bState.h_half && bState.v_half:
+					pel += (int32(bState.channel[i]) +
+						int32(bState.channel[i+bState.stride])) / 2
+
+				case bState.h_half && bState.v_half:
+					pel += (int32(bState.channel[i]) +
+						int32(bState.channel[i+1]) +
+						int32(bState.channel[i+bState.stride]) +
+						int32(bState.channel[i+bState.stride+1])) / 4
+				}
+			}
+			b[v*8+u] += pel >> (samples - 1)
 		}
 	}
 }
