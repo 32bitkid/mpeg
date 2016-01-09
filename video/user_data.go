e635d9eb36f2580070aed49ab9e7fe7f55fdff8a
James Holmes <32bitkid@gmail.com>
2016-01-08 21:45:34 -0700

    compressing ifs

--- video/user_data.go
+++ video/user_data.go
@@ -3,31 +3,25 @@ package video
 type UserData []byte
 
 func (br *VideoSequence) user_data() (UserData, error) {
-	err := UserDataStartCode.assert(br)
-	if err != nil {
+	if err := UserDataStartCode.assert(br); err != nil {
 		return nil, err
 	}
 
 	data := make(UserData, 0)
 
 	for {
-		peek, err := br.Peek32(24)
-		if err != nil {
+		if peek, err := br.Peek32(24); err != nil {
 			return nil, err
-		}
-
-		if peek == StartCodePrefix {
+		} else if peek == StartCodePrefix {
 			break
 		}
 
-		raw, err := br.Read32(8)
-		if err != nil {
+		if raw, err := br.Read32(8); err != nil {
 			return nil, err
+		} else {
+			data = append(data, byte(raw))
 		}
-
-		data = append(data, byte(raw))
 	}
 
 	return data, next_start_code(br)
-
 }
