1222a426fa530a967974b042cf2d3f24c0707ec4
James Holmes <32bitkid@gmail.com>
2016-01-13 10:26:36 -0700

    cleaning up start code consts.

--- video/start_codes.go
+++ video/start_codes.go
@@ -8,29 +8,17 @@ type StartCode uint32
 const (
 	StartCodePrefix = 0x000001
 
-	pictureCode        = 0x00
-	reservedCode1      = 0xB0
-	reservedCode2      = 0xB1
-	userDataCode       = 0xB2
-	sequenceHeaderCode = 0xB3
-	sequenceErrorCode  = 0xB4
-	extensionCode      = 0xB5
-	reservedCode3      = 0xB6
-	sequenceEndCode    = 0xB7
-	groupCode          = 0xB8
-
-	SequenceHeaderStartCode StartCode = (StartCodePrefix << 8) + sequenceHeaderCode
-	ExtensionStartCode      StartCode = (StartCodePrefix << 8) + extensionCode
-	SequenceEndStartCode    StartCode = (StartCodePrefix << 8) + sequenceEndCode
-	GroupStartCode          StartCode = (StartCodePrefix << 8) + groupCode
-	PictureStartCode        StartCode = (StartCodePrefix << 8) + pictureCode
-	UserDataStartCode       StartCode = (StartCodePrefix << 8) + userDataCode
+	PictureStartCode StartCode = (StartCodePrefix << 8) | 0x00
 
 	// slice_start_code 01 through AF
-	MinSliceStartCode StartCode = (StartCodePrefix << 8) + 0x01
-	MaxSliceStartCode StartCode = (StartCodePrefix << 8) + 0xAF
+	MinSliceStartCode StartCode = (StartCodePrefix << 8) | 0x01
+	MaxSliceStartCode StartCode = (StartCodePrefix << 8) | 0xAF
 
-	// system start codes (see note) B9 through FF
+	UserDataStartCode       StartCode = (StartCodePrefix << 8) | 0xB2
+	SequenceHeaderStartCode StartCode = (StartCodePrefix << 8) | 0xB3
+	ExtensionStartCode      StartCode = (StartCodePrefix << 8) | 0xB5
+	SequenceEndStartCode    StartCode = (StartCodePrefix << 8) | 0xB7
+	GroupStartCode          StartCode = (StartCodePrefix << 8) | 0xB8
 )
 
 func (code StartCode) IsSlice() bool {
