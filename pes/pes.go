1b3370ab0919fd50d803be88d9638d267f81add8
James Holmes <32bitkid@gmail.com>
2016-01-14 14:25:02 -0700

    updating disclaimer

--- pes/pes.go
+++ pes/pes.go
@@ -1,6 +1,9 @@
 // Package pes implements an MPEG-2 packetized elementary stream parser as defined in ISO/IEC 13818-1.
 //
-// *Note* This package is a work-in-progress and, even when complete, is intended for educational purposes only.
+// This package is experimental and is not intended for
+// use in production environments.
+//
+//*USE AT YOUR OWN RISK*
 //
 // This package is _not_ optimized for processing video in a production environment, instead is
 // geared towards an educational look at some of the algorithms and processes used to decode
