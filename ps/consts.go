1198a70c545e7f0dcd13d78670db013bae84a218
James Holmes <32bitkid@gmail.com>
2016-01-18 19:56:33 -0700

    clearer intent

--- ps/consts.go
+++ ps/consts.go
@@ -5,11 +5,7 @@ const StartCodePrefix uint32 = 0x000001
 type StartCode uint32
 
 const (
-	packCode         uint32 = 0xba
-	programEnd       uint32 = 0xb9
-	systemHeaderCode uint32 = 0xbb
-
-	PackStartCode         StartCode = StartCode((StartCodePrefix << 8) | packCode)
-	ProgramEndCode        StartCode = StartCode((StartCodePrefix << 8) | programEnd)
-	SystemHeaderStartCode StartCode = StartCode((StartCodePrefix << 8) | systemHeaderCode)
+	PackStartCode         StartCode = (StartCodePrefix << 8) | 0xBA
+	ProgramEndCode        StartCode = (StartCodePrefix << 8) | 0xB9
+	SystemHeaderStartCode StartCode = (StartCodePrefix << 8) | 0xBB
 )
