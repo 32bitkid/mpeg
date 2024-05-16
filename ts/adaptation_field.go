e027e00f3ef1d2e6d367d17465e16f93e2654d86
Potter Xu <xujingchuan1995@gmail.com>
2024-05-16 16:04:52 +0800

    support parse TransportPrivateData and AdapatationExtension

--- ts/adaptation_field.go
+++ ts/adaptation_field.go
@@ -18,6 +18,110 @@ const (
 	FieldThenPayload                        // 0b11
 )
 
+type TransportPrivateData struct {
+	// length of the transport private data section excludes the length byte
+	length uint32
+	data   []byte
+}
+
+func newTransportPrivateData(br bitreader.BitReader) (*TransportPrivateData, uint32, error) {
+	tpd := TransportPrivateData{}
+	var err error
+	if tpd.length, err = br.Read32(8); err != nil {
+		return nil, 0, err
+	}
+	if tpd.length > 0 {
+		tpd.data = make([]byte, tpd.length)
+		_, err = io.ReadFull(br, tpd.data)
+		if err == io.EOF {
+			return nil, 0, io.ErrUnexpectedEOF
+		} else if err != nil {
+			return nil, 0, err
+		}
+	}
+	return &tpd, tpd.length, nil
+}
+
+type AdaptationExt struct {
+	LegalTimeWindowFlag bool
+	PiecewiseRateFlag   bool
+	SeamlessSpliceFlag  bool
+
+	// optional fields
+	LTWValidFlag  bool
+	LTWOffset     uint16
+	PiecewiseRate uint32
+	SpliceType    uint8
+	DTSNextAU     uint64
+
+	// length of the adaptation extension section excludes the length byte
+	length uint32
+}
+
+func newAdaptationExt(br bitreader.BitReader) (*AdaptationExt, uint32, error) {
+	ae := AdaptationExt{}
+	var err error
+	if ae.length, err = br.Read32(8); err != nil {
+		return nil, 0, err
+	}
+	remainLength := ae.length
+
+	if remainLength >= 1 {
+		if ae.LegalTimeWindowFlag, err = br.Read1(); err != nil {
+			return nil, 0, err
+		}
+		if ae.PiecewiseRateFlag, err = br.Read1(); err != nil {
+			return nil, 0, err
+		}
+		if ae.SeamlessSpliceFlag, err = br.Read1(); err != nil {
+			return nil, 0, err
+		}
+		br.Skip(5)
+		remainLength--
+	}
+
+	if remainLength >= 2 && ae.LegalTimeWindowFlag {
+		if ae.LTWValidFlag, err = br.Read1(); err != nil {
+			return nil, 0, err
+		}
+		if ae.LTWOffset, err = br.Read16(15); err != nil {
+			return nil, 0, err
+		}
+		remainLength -= 2
+	}
+
+	if remainLength >= 3 && ae.PiecewiseRateFlag {
+		br.Skip(2)
+		if ae.PiecewiseRate, err = br.Read32(22); err != nil {
+			return nil, 0, err
+		}
+		remainLength -= 3
+	}
+
+	if remainLength >= 5 && ae.SeamlessSpliceFlag {
+		if ae.SpliceType, err = br.Read8(4); err != nil {
+			return nil, 0, err
+		}
+		var v uint64
+		if v, err = br.Read64(3); err != nil {
+			return nil, 0, err
+		}
+		ae.DTSNextAU = v << 30
+		br.Skip(1)
+		if v, err = br.Read64(15); err != nil {
+			return nil, 0, err
+		}
+		ae.DTSNextAU += v << 15
+		br.Skip(1)
+		if v, err = br.Read64(15); err != nil {
+			return nil, 0, err
+		}
+		ae.DTSNextAU += v
+	}
+
+	return &ae, ae.length, nil
+}
+
 // AdaptationField is an optional field in a transport stream packet header.
 type AdaptationField struct {
 	DiscontinuityIndicator            bool
@@ -29,13 +133,14 @@ type AdaptationField struct {
 	TransportPrivateDataFlag          bool
 	AdaptationFieldExtensionFlag      bool
 
-	PCR             uint64
-	OPCR            uint64
-	SpliceCountdown uint8
-	// TODO: implement these
-	// TransportPrivateData *TransportPrivateData
-	// AdapatationExtension *AdaptationExt
+	// optional fields
+	PCR                  uint64
+	OPCR                 uint64
+	SpliceCountdown      uint8
+	TransportPrivateData *TransportPrivateData
+	AdapatationExtension *AdaptationExt
 
+	// length of the adaptation field section excludes the length byte
 	length uint32
 	junk   []byte
 }
@@ -111,6 +216,22 @@ func newAdaptationField(br bitreader.BitReader) (*AdaptationField, uint32, error
 			return nil, 0, err
 		}
 		remainLength--
+	}
+
+	if remainLength >= 1 && af.TransportPrivateDataFlag {
+		var length uint32
+		if af.TransportPrivateData, length, err = newTransportPrivateData(br); err != nil {
+			return nil, 0, err
+		}
+		remainLength -= length + 1
+	}
+
+	if remainLength >= 2 && af.AdaptationFieldExtensionFlag {
+		var length uint32
+		if af.AdapatationExtension, length, err = newAdaptationExt(br); err != nil {
+			return nil, 0, err
+		}
+		remainLength -= length + 1
 	}
 
 	// skip the remaining bytes
